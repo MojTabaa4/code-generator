93c3cd220b65a372d87f5259e66ef3220b68ff3f
Mojtaba Abtahi <mojtabablue@gmail.com>
2023-05-30 18:32:21 +0330

    add training model

---
+++ model/train.py
@@ -0,0 +1,85 @@
+import os
+
+from datasets import load_dataset
+from tokenizers import ByteLevelBPETokenizer
+from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer, DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+os.environ["WANDB_DISABLED"] = "true"
+
+
+def encode(lines):
+    return tokenizer(lines['text'], add_special_tokens=True, truncation=True, max_length=512)
+
+
+TRAIN_BASE = False
+TOKENIZER_DIR = "../tokenizer"
+
+paths = ["../data.txt"]
+
+if TRAIN_BASE:
+    tokenizer = ByteLevelBPETokenizer()
+
+    tokenizer.train(files=paths, vocab_size=52000, min_frequency=2, special_tokens=[
+        "<s>",
+        "<pad>",
+        "</s>",
+        "<unk>",
+        "<mask>",
+    ])
+
+    tokenizer.save_model(TOKENIZER_DIR)
+
+inp = "print('hello world!')"
+
+tokenizer = GPT2Tokenizer.from_pretrained(TOKENIZER_DIR)
+tokenizer.add_special_tokens({
+    "eos_token": "</s>",
+    "bos_token": "<s>",
+    "unk_token": "<unk>",
+    "pad_token": "<pad>",
+    "mask_token": "<mask>"
+})
+
+t = tokenizer.encode(inp)
+print(t)
+print(tokenizer.decode(t))
+
+config = GPT2Config(
+    vocab_size=tokenizer.vocab_size,
+    bos_token=tokenizer.bos_token_id,
+    eos_token=tokenizer.eos_token_id
+)
+
+model = GPT2LMHeadModel(config)
+
+dataset = load_dataset("text", data_files=paths)
+
+dataset.set_transform(encode)
+dataset = dataset['train']
+
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
+
+if not os.path.exists("../GPyT"):
+    os.makedirs("../GPyT")
+
+training_args = TrainingArguments(
+    output_dir="../GPyT",
+    overwrite_output_dir=True,
+    num_train_epochs=1,
+    per_device_train_batch_size=4,
+    save_steps=100,
+    save_total_limit=2,
+    prediction_loss_only=True,
+    remove_unused_columns=False
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=dataset
+)
+
+trainer.train()
+trainer.save_model("GPyT")
